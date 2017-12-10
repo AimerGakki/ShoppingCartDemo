6a1b9598ea4c2380649a9ac0092886ad5298d798
AimerGakki <504811154@qq.com>
2017-12-10 14:01:29 +0800

    地址页面功能添加

--- js/address.js
+++ js/address.js
@@ -1,24 +1,52 @@
 new Vue({
     el: '.container',
     data: {
-        addressList:[]
+        addressList: [],
+        limitNumber: 3,
+        currentIndex: 0,
+        delAddressFlag: false,
+        curAddress: '',
+        shippingMethod: 1
 
     },
     mounted: function(){
         this.$nextTick(function() {
-			getAddress();
-		  })
+			this.getAddress();
+		  });
+    },
+    computed: {
+        filteredItems: function(){
+            return this.addressList.slice(0,this.limitNumber);
+        }
     },
     methods: {
         getAddress: function() {
             let _this =this;
-            _this.$http.get('data/address.json',{id:456}).then(response => {
+            this.$http.get('data/address.json',{id:456}).then(response => {
                 var res = response.data;
-                if (res.status == 0) {
+                if (res.status == '0') {
                     _this.addressList = res.result;
                 }
                 
             })
+        },
+        setDefalut: function(addressId) {
+            this.addressList.forEach((address,index) => {
+                if(address.addressId==addressId){
+                    address.isDefault = true;
+                } else{
+                    address.isDefault = false;
+                }
+            });
+        },
+        delConfirm: function(item) {
+            this.delAddressFlag = true;
+            this.curAddress = item;
+        },
+        delAddress: function() {
+            var index = this.addressList.indexOf(this.curAddress);
+            this.addressList.splice(index,1);
+            this.delAddressFlag = false;
         }
     }
 })
